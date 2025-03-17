404f3c430e04b93eadc80c014a59dae9f286b1c4
iFightDucks <108614566+iFightDucks@users.noreply.github.com>
2025-03-17 15:35:33 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,16 +3,20 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
     email: {
         type: String,
         required: true,
         unique: true,
         trim: true,
         lowercase: true,
-        minLength: [ 6, 'Email must be at least 6 characters long' ],
-        maxLength: [ 50, 'Email must not be longer than 50 characters' ]
+        minLength: [6, 'Email must be at least 6 characters long'],
+        maxLength: [50, 'Email must not be longer than 50 characters']
     },
-
     password: {
         type: String,
         select: false,
